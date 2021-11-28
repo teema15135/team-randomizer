70646fd176a24fabd04cc333c0932501ad1f29e6
Teema Luksameset <tm@odds.team>
2021-11-29 01:46:41 +0700

    Initial project

---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="container" class="grid gap-6 w-8/12 h-2/6">
+    <div
+      id="input-container"
+      class="bg-white rounded-2xl p-8 w-full h-full col-span-2 shadow-md"
+    >
+      <textarea
+        placeholder="Enter name separate by new line..."
+        class="bg-transparent placeholder- text-prussianBlue resize-none overflow-auto focus:outline-none w-full h-full"
+        v-model="nameString"
+      ></textarea>
+    </div>
+    <hr class="text-articLime border-articLime border-2 rounded-full col-span-2 shadow-md">
+    <div class="team-container team-a bg-prussianBlue">
+      <h1 class="bold">Team A</h1>
+      <hr class="text-white my-2" />
+      <ul id="team-a">
+        <li v-for="name in nameArray.a" :key="name">{{ name }}</li>
+      </ul>
+    </div>
+    <div class="team-container team-b bg-bittersweet">
+      <h1 class="bold">Team B</h1>
+      <hr class="text-white my-2" />
+      <ul id="team-b">
+        <li v-for="name in nameArray.b" :key="name">{{ name }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      nameString: ''
+    }
+  },
+  computed: {
+    nameArray: function(): { a: string[]; b: string[] } {
+      let totalArray = [...new Set(this.nameString.split('\n'))]
+        .filter(value => {
+          return value.trim() != ''
+        })
+        .sort(() => Math.random() - 0.5)
+      let half = Math.ceil(totalArray.length / 2)
+      return {
+        a: totalArray.slice(0, half),
+        b: totalArray.slice(half)
+      }
+    }
+  }
+})
+</script>
+
+<style>
+#container {
+  @apply min-h-screen mx-auto p-12;
+  grid-template-rows: 1fr 1px 1fr;
+}
+
+.team-container {
+  @apply px-8 py-6 text-white rounded-2xl shadow-md;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    colors: {
+      transparent: "transparent",
+      prussianBlue: "#0b3954",
+      beauBlue: "#bfd7ea",
+      bittersweet: "#ff6663",
+      articLime: "#e0ff4f",
+      white: "#fefffe",
+    }
+  },
+  variants: {},
+  plugins: []
+}
