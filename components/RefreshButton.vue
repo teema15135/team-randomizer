4801c666df93912ce87a2f05f6d90ed1518baf3c
Teema Luksameset <tm@odds.team>
2021-11-30 00:01:12 +0700

    Reposition re-random button

--- components/RefreshButton.vue
+++ components/RefreshButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrap flex flex-row items-center cursor-pointer">
+  <div class="wrap flex flex-row justify-center items-center cursor-pointer">
     <RefreshIcon id="refresh-icon" class="inner w-6 h-6 text-prussianBlue" />
     <a id="refresh-text" class="text-prussianBlue ml-1 ">
       Random again
@@ -9,17 +9,15 @@
 
 <style>
 
-.wrap:hover > #refresh-icon {
-  transform: rotate(-180deg) scale(1.25);
+.wrap:hover {
+  transform: scale(1.25);
 }
 
-.wrap:hover > #refresh-text {
-  transform: scale(1.25);
-  @apply ml-2;
+.wrap:hover > #refresh-icon {
+  transform: rotate(-180deg) ;
 }
 
 #refresh-text {
-  transform-origin: center left;
   user-select: none;
   text-shadow: 0 0 2px #bfd7ea;
 }
@@ -28,7 +26,7 @@
   filter: drop-shadow(0 0 2px #bfd7ea);
 }
 
-#refresh-icon, #refresh-text {
+.wrap, #refresh-icon, #refresh-text {
   transition: transform 0.325s, margin-left 0.325s;
 }
 </style>
